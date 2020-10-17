99a06dc913d5791b4011d5dda5eafe0ee7596bfe
MartinKrause21 <martin.21.krause@gmail.com>
2020-10-17 19:24:32 +0200

    Add function to work with names

--- Open-Lab-02.09/Names.cs
+++ Open-Lab-02.09/Names.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_02._09
     {
         public string ConcatName(string firstName, string lastName)
         {
-            throw new NotImplementedException();
+            
+
+            string ContactName = (lastName + ", " + firstName);
+            return ContactName;
+           
+
+
         }
     }
 }
